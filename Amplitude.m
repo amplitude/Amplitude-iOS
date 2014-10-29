f065cf2ba1dcc4ded559c48a3962aa93d7195dbb
Curtis Liu <curtis@amplitude.com>
2014-10-29 15:29:16 -0700

    Remove unused methods getPlatformString and getPhoneModel

--- Amplitude.m
+++ Amplitude.m
@@ -900,52 +900,6 @@ static BOOL useAdvertisingIdForDeviceId = NO;
     }
 }
 
-// Taken from http://stackoverflow.com/a/3950748/340520
-+ (NSString *)getPlatformString
-{
-    size_t size;
-    sysctlbyname("hw.machine", NULL, &size, NULL, 0);
-    char *machine = malloc(size);
-    sysctlbyname("hw.machine", machine, &size, NULL, 0);
-    NSString *platform = [NSString stringWithUTF8String:machine];
-    free(machine);
-    return platform;
-}
-
-+ (NSString *)getPhoneModel{
-    NSString *platform = [self getPlatformString];
-    if ([platform isEqualToString:@"iPhone1,1"])    return @"iPhone 1";
-    if ([platform isEqualToString:@"iPhone1,2"])    return @"iPhone 3G";
-    if ([platform isEqualToString:@"iPhone2,1"])    return @"iPhone 3GS";
-    if ([platform isEqualToString:@"iPhone3,1"])    return @"iPhone 4";
-    if ([platform isEqualToString:@"iPhone3,3"])    return @"iPhone 4";
-    if ([platform isEqualToString:@"iPhone4,1"])    return @"iPhone 4S";
-    if ([platform isEqualToString:@"iPhone5,1"])    return @"iPhone 5";
-    if ([platform isEqualToString:@"iPhone5,2"])    return @"iPhone 5";
-    if ([platform isEqualToString:@"iPod1,1"])      return @"iPod Touch 1G";
-    if ([platform isEqualToString:@"iPod2,1"])      return @"iPod Touch 2G";
-    if ([platform isEqualToString:@"iPod3,1"])      return @"iPod Touch 3G";
-    if ([platform isEqualToString:@"iPod4,1"])      return @"iPod Touch 4G";
-    if ([platform isEqualToString:@"iPod5,1"])      return @"iPod Touch 5G";
-    if ([platform isEqualToString:@"iPad1,1"])      return @"iPad 1";
-    if ([platform isEqualToString:@"iPad2,1"])      return @"iPad 2";
-    if ([platform isEqualToString:@"iPad2,2"])      return @"iPad 2";
-    if ([platform isEqualToString:@"iPad2,3"])      return @"iPad 2";
-    if ([platform isEqualToString:@"iPad2,4"])      return @"iPad 2";
-    if ([platform isEqualToString:@"iPad2,5"])      return @"iPad Mini";
-    if ([platform isEqualToString:@"iPad2,6"])      return @"iPad Mini";
-    if ([platform isEqualToString:@"iPad2,7"])      return @"iPad Mini";
-    if ([platform isEqualToString:@"iPad3,1"])      return @"iPad 3";
-    if ([platform isEqualToString:@"iPad3,2"])      return @"iPad 3";
-    if ([platform isEqualToString:@"iPad3,3"])      return @"iPad 3";
-    if ([platform isEqualToString:@"iPad3,4"])      return @"iPad 4";
-    if ([platform isEqualToString:@"iPad3,5"])      return @"iPad 4";
-    if ([platform isEqualToString:@"iPad3,6"])      return @"iPad 4";
-    if ([platform isEqualToString:@"i386"])         return @"Simulator";
-    if ([platform isEqualToString:@"x86_64"])       return @"Simulator";
-    return platform;
-}
-
 + (NSString*)md5HexDigest:(NSString*)input
 {
     const char* str = [input UTF8String];
@@ -966,7 +920,7 @@ static BOOL useAdvertisingIdForDeviceId = NO;
     if (![fileManager fileExistsAtPath:to] &&
         [fileManager fileExistsAtPath:from]) {
         if ([fileManager copyItemAtPath:from toPath:to error:&error]) {
-            //NSLog(@"INFO: copied %@ to %@", from, to);
+            AMPLITUDE_LOG(@"INFO: copied %@ to %@", from, to);
             [fileManager removeItemAtPath:from error:NULL];
         } else {
             AMPLITUDE_LOG(@"WARN: Copy from %@ to %@ failed: %@", from, to, error);
