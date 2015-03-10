9e75d3849f7594b63a37126ae760429c6a8d5056
Allan Carroll <allanca@gmail.com>
2015-03-10 16:45:33 -0700

    Move class variables to instance variables.
    Needed for testing to work.
    Also, cleaned up reference counting a bit.

--- AmplitudeTests/InitializeTests.m
+++ AmplitudeTests/InitializeTests.m
@@ -11,11 +11,6 @@
 #import <OCMock/OCMock.h>
 #import "Amplitude.h"
 
-
-@interface InitializeTests : XCTestCase
-
-@end
-
 @interface Amplitude (Test)
 
 @property NSOperationQueue *backgroundQueue;
@@ -43,32 +38,35 @@
 
 @end
 
-@implementation InitializeTests
+@interface InitializeTests : XCTestCase
 
-Amplitude *amplitude;
+@end
+
+@implementation InitializeTests {
+    Amplitude *amplitude;
+    id _archivedObj;
+    id _partialMock;
+}
 
 NSString *const apiKey = @"000000";
 NSString *const userId = @"userId";
 
-id archivedObj;
-id partialMock;
-
 - (BOOL) archive:(id)rootObject toFile:(NSString *)path {
-    archivedObj = rootObject;
+    _archivedObj = rootObject;
     return TRUE;
 }
 
 - (id) unarchive:(NSString *)path {
-    return archivedObj;
+    return _archivedObj;
 }
 
 - (void)setUp {
     [super setUp];
     amplitude = [Amplitude alloc];
     // Mock the methods before init
-    partialMock = OCMPartialMock(amplitude);
-    OCMStub([partialMock archive:[OCMArg any] toFile:[OCMArg any]]).andCall(self, @selector(archive:toFile:));
-    OCMStub([partialMock unarchive:[OCMArg any]]).andCall(self, @selector(unarchive:));
+    _partialMock = OCMPartialMock(amplitude);
+    OCMStub([_partialMock archive:[OCMArg any] toFile:[OCMArg any]]).andCall(self, @selector(archive:toFile:));
+    OCMStub([_partialMock unarchive:[OCMArg any]]).andCall(self, @selector(unarchive:));
     [amplitude init];
 }
 
