7730496834b354220d68dce4e9bfb8f4752205aa
Daniel Jih <daniel@amplitude.com>
2015-08-03 13:38:32 -0700

    clean up amplitude in base tear down

--- AmplitudeTests/BaseTestCase.m
+++ AmplitudeTests/BaseTestCase.m
@@ -13,6 +13,7 @@
 #import "Amplitude+SSLPinning.h"
 #import "Amplitude+Test.h"
 #import "BaseTestCase.h"
+#import "AMPARCMacros.h"
 
 NSString *const apiKey = @"000000";
 NSString *const userId = @"userId";
@@ -40,6 +41,7 @@ NSString *const userId = @"userId";
     [self.amplitude flushQueueWithQueue:self.amplitude.initializerQueue];
     [self.amplitude flushQueue];
     [_partialMock stopMocking];
+    SAFE_ARC_RELEASE(self.amplitude);
     [super tearDown];
 }
 
