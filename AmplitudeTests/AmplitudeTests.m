a570d0834b10e9714765255fb8ed94ed10275e1c
Daniel Jih <daniel@amplitude.com>
2015-08-07 13:43:32 -0700

    added AmplitudeTests

---
+++ AmplitudeTests/AmplitudeTests.m
@@ -0,0 +1,29 @@
+//
+//  AmplitudeTests.m
+//  Amplitude
+//
+//  Created by Daniel Jih on 8/7/15.
+//  Copyright (c) 2015 Amplitude. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <UIKit/UIKit.h>
+#import <OCMock/OCMock.h>
+#import "Amplitude.h"
+#import "AMPConstants.h"
+#import "Amplitude+Test.h"
+#import "BaseTestCase.h"
+#import "AMPDeviceInfo.h"
+#import "AMPARCMacros.h"
+
+@interface AmplitudeTests : BaseTestCase
+
+@end
+
+@implementation AmplitudeTests {
+    AMPDeviceInfo *_deviceInfo;
+}
+
+
+
+@end
