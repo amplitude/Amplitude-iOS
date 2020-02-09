4b99afee70dc91f95d8f6153d517038014be81d2
haoliu <hao.liu@amplitude.com>
2020-02-08 22:24:03 -0800

    Updated some project settings and add marcos for AMPLocationManagerDelegate.m

--- Sources/Amplitude/AMPLocationManagerDelegate.m
+++ Sources/Amplitude/AMPLocationManagerDelegate.m
@@ -14,10 +14,14 @@
 }
 
 - (void)locationManager:(CLLocationManager*) manager didChangeAuthorizationStatus:(CLAuthorizationStatus) status {
-    // kCLAuthorizationStatusAuthorized is deprecated in iOS 8. Add support for
-    // the new location authorization types if we're compiling for iOS 8 or higher.
+#if TARGET_OS_IPHONE
+// kCLAuthorizationStatusAuthorized is deprecated in iOS 8. Add support for
+// the new location authorization types if we're compiling for iOS 8 or higher.
 #ifdef __IPHONE_8_0
     if (status == kCLAuthorizationStatusAuthorizedAlways || status == kCLAuthorizationStatusAuthorizedWhenInUse) {
+#else
+    if (status == kCLAuthorizationStatusAuthorized) {
+#endif
 #else
     if (status == kCLAuthorizationStatusAuthorized) {
 #endif
@@ -25,5 +29,4 @@
         [Amplitude performSelector:updateLocation];
     }
 }
-
 @end
