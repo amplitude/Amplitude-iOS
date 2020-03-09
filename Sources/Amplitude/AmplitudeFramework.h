1a2159765cfc7d9a214a837f1736978335388551
haoliu <hao.liu@amplitude.com>
2020-03-09 16:43:03 -0700

    Adjusted public headers declared in umbrella header

--- Sources/Amplitude/AmplitudeFramework.h
+++ Sources/Amplitude/AmplitudeFramework.h
@@ -14,16 +14,7 @@ FOUNDATION_EXPORT const unsigned char AmplitudeVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Amplitude/PublicHeader.h>
 
-#import <Amplitude/AMPConstants.h>
-#import <Amplitude/AMPDatabaseHelper.h>
-#import <Amplitude/AMPDeviceInfo.h>
-#import <Amplitude/AMPIdentify.h>
 #import <Amplitude/Amplitude.h>
-#import <Amplitude/Amplitude+SSLPinning.h>
-#import <Amplitude/AMPLocationManagerDelegate.h>
+#import <Amplitude/AMPIdentify.h>
 #import <Amplitude/AMPRevenue.h>
-#import <Amplitude/AMPURLConnection.h>
-#import <Amplitude/AMPUtils.h>
-#import <Amplitude/ISPCertificatePinning.h>
-#import <Amplitude/ISPPinnedNSURLConnectionDelegate.h>
-#import <Amplitude/ISPPinnedNSURLSessionDelegate.h>
+#import <Amplitude/Amplitude+SSLPinning.h>
