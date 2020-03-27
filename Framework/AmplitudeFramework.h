21b1989a2d81db2665ee460e85d84e19ff07f33b
haoliu <hao.liu@amplitude.com>
2020-03-26 22:59:44 -0700

    Added umbrella header file as public header

--- Framework/AmplitudeFramework.h
+++ Framework/AmplitudeFramework.h
@@ -1,19 +1,4 @@
-#if TARGET_OS_IPHONE
-#import <UIKit/UIKit.h>
-#elif TARGET_OS_OSX
-#import <Cocoa/Cocoa.h>
-#endif
-
-#import <Foundation/Foundation.h>
-
-//! Project version number for Amplitude.
-FOUNDATION_EXPORT double AmplitudeVersionNumber;
-
-//! Project version string for Amplitude.
-FOUNDATION_EXPORT const unsigned char AmplitudeVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Amplitude/PublicHeader.h>
-
+// All Amplitude public headers
 #import <Amplitude/Amplitude.h>
 #import <Amplitude/AMPIdentify.h>
 #import <Amplitude/AMPRevenue.h>
