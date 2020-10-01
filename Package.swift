c693724c2c254eb61c9c29b88d1408adb9577964
haoliu <hao.liu@amplitude.com>
2020-09-30 19:13:41 -0700

    Fix package.swift

--- Package.swift
+++ Package.swift
@@ -5,15 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "Amplitude",
+    platforms: [
+        .iOS(.v10),
+        .tvOS(.v9)
+        .macOS(.v10_10)
+    ],
     products: [
-        .library(
-            name: "Amplitude",
-            targets: ["Amplitude"]),
+        .library(name: "Amplitude", targets: ["Amplitude"]),
     ],
     targets: [
         .target(
             name: "Amplitude",
+            path: "Sources/Amplitude",
+            resources: [
+                .process("EventExplorer/Resources")
+            ]
             exclude: [],
-            publicHeadersPath: "."),
+            publicHeadersPath: ".",
+            cSettings: [.headerSearchPath("."),
+                        .headerSearchPath("EventExplorer"),
+                        .headerSearchPath("SSLPinning")]),
     ]
 )
