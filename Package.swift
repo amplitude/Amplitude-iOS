a66d6e961b24afbce76fe7039c46a46486b772ff
John Tran <jptran318@gmail.com>
2021-03-05 17:27:11 -0800

    remove event explorer package configs

--- Package.swift
+++ Package.swift
@@ -14,35 +14,10 @@ let package = Package(
         .library(name: "Amplitude", targets: ["Amplitude"]),
     ],
     targets: [
-          .target(
-            name: "EventExplorer",
-            path: "Sources/EventExplorer",
-            resources: [
-                .process("Resources/AMPBubbleView.xib"),
-                .process("Resources/AMPInfoViewController.xib"),
-                .process("Resources/Images/cancel.png"),
-                .process("Resources/Images/cancel@2x.png"),
-                .process("Resources/Images/cancel@3x.png"),
-                .process("Resources/Images/logo-banner.png"),
-                .process("Resources/Images/logo-banner@2x.png"),
-                .process("Resources/Images/logo-banner@3x.png"),
-                .process("Resources/Images/logo-button.png"),
-                .process("Resources/Images/logo-button@2x.png"),
-                .process("Resources/Images/logo-button@3x.png")
-            ],
-            publicHeadersPath: ".",
-            cSettings: [
-               .headerSearchPath("../Amplitude")
-            ]
-          ),
-          .target(
+        .target(
             name: "Amplitude",
-             dependencies: [
-                .target(name: "EventExplorer", condition: .when(platforms: [.iOS])),
-            ],
             path: "Sources/Amplitude",
             resources: [.process("Resources/ComodoRsaDomainValidationCA.der")],
             publicHeadersPath: "."),
     ]
 )
-
