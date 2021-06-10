0ab8b83b256c12d25f68d6b2d5fa7783043e7278
Dalton Claybrook <daltonclaybrook@gmail.com>
2021-06-09 23:13:01 -0400

    fix: Add watchOS scheme for building with Carthage (#351)
    * Add the watchOS scheme as a shared scheme
    
    * Add background notifier to umbrella header for watchOS

--- Framework/AmplitudeFramework.h
+++ Framework/AmplitudeFramework.h
@@ -4,3 +4,7 @@
 #import <Amplitude/AMPRevenue.h>
 #import <Amplitude/AMPTrackingOptions.h>
 #import <Amplitude/Amplitude+SSLPinning.h>
+
+#if TARGET_OS_WATCH
+#import <Amplitude/AMPBackgroundNotifier.h>
+#endif
