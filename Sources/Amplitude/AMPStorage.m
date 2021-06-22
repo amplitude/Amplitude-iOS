c44d1f11a3db4029a4fd2ab80bc276b19d791813
Dante Tam <dante.tam1@gmail.com>
2021-06-21 17:24:55 -0700

    Change the copyright

--- Sources/Amplitude/AMPStorage.m
+++ Sources/Amplitude/AMPStorage.m
@@ -1,6 +1,6 @@
 //
 //  Amplitude.m
-//  Copyright (c) 2013 Amplitude Inc. (https://amplitude.com/)
+//  Copyright (c) 2021 Amplitude Inc. (https://amplitude.com/)
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
