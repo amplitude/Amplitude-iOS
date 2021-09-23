468387454a7b9a599bc03b058e9a6c5f062dbcdb
yuhao900914 <yuhao900914@gmail.com>
2021-09-23 12:30:50 -0700

    chore: update the release config to enable do release on add-observe-support branch

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "branches": ["main", "initializeCallback"],
+  "branches": ["main", "add-observe-support"],
   "plugins": [
     ["@semantic-release/commit-analyzer", {
       "preset": "angular",
