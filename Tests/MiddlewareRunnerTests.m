0527f74d4e25619a71f1be1e7afd7ff2a01688c7
Qingzhuo Zhen <qingzhuo.zhen@amplitude.com>
2021-11-08 15:37:52 -0800

    test: add middleware unit tests

--- Tests/MiddlewareRunnerTests.m
+++ Tests/MiddlewareRunnerTests.m
@@ -10,18 +10,9 @@
 #import "AMPMiddleware.h"
 #import "AMPMiddlewareRunner.h"
 
-AMPMiddlewareBlock customizeBlock = ^(AMPMiddlewarePayload *_Nonnull payload, AMPMiddlewareNext _Nonnull next) {
-    next(payload);
-};
-
-AMPBlockMiddleware *customizeAllTrackCalls = [[AMPBlockMiddleware alloc] initWithBlock:^(AMPMiddlewarePayload * _Nonnull payload, AMPMiddlewareNext  _Nonnull next) {
-    [payload.extra setValue:@"test" forKey:@"event_type"];
-    next(payload);
-}];
-
-
-
 @interface MiddlewareRunnerTests : XCTestCase
+
+@property AMPMiddlewareRunner *middlewareRunner;
 
 @end
 
@@ -29,23 +20,71 @@ AMPBlockMiddleware *customizeAllTrackCalls = [[AMPBlockMiddleware alloc] initWit
 @implementation MiddlewareRunnerTests
 
 - (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    _middlewareRunner = [AMPMiddlewareRunner middleRunner];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    _middlewareRunner = nil;
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+- (void)testMiddlewareRun {
+    NSString *eventType = @"middleware event";
+    AMPBlockMiddleware *updateEventTypeMiddleware = [[AMPBlockMiddleware alloc] initWithBlock: ^(AMPMiddlewarePayload * _Nonnull payload, AMPMiddlewareNext _Nonnull next) {
+        [payload.event setValue:eventType forKey:@"event_type"];
+        next(payload);
     }];
+    NSString *deviceModel = @"middleware_device";
+    AMPBlockMiddleware *updateDeviceModelMiddleware = [[AMPBlockMiddleware alloc] initWithBlock: ^(AMPMiddlewarePayload * _Nonnull payload, AMPMiddlewareNext _Nonnull next) {
+        [payload.event setValue:deviceModel forKey:@"device_model"];
+        next(payload);
+    }];
+    [_middlewareRunner add:updateEventTypeMiddleware];
+    [_middlewareRunner add:updateDeviceModelMiddleware];
+    
+    NSMutableDictionary *event = [NSMutableDictionary dictionary];
+    [event setValue:@"sample_event" forKey:@"event_type"];
+    [event setValue:@"sample_device" forKey:@"device_model"];
+    NSMutableDictionary *extra = [NSMutableDictionary dictionary];
+    
+    AMPMiddlewarePayload * middlewarePayload = [[AMPMiddlewarePayload alloc] initWithEvent:event extra:extra];
+    
+    __block BOOL middlewareCompleted = NO;
+    
+    [_middlewareRunner run:middlewarePayload next:^(AMPMiddlewarePayload *_Nullable newPayload){
+        middlewareCompleted = YES;
+    }];
+    
+    XCTAssertEqual(middlewareCompleted, YES);
+    XCTAssertEqualObjects([event objectForKey:@"event_type"], eventType);
+    XCTAssertEqualObjects([event objectForKey:@"device_model"], deviceModel);
+}
+
+- (void)testRunWithNotPassMiddleware {
+    NSString *eventType = @"middleware event";
+    AMPBlockMiddleware *updateEventTypeMiddleware = [[AMPBlockMiddleware alloc] initWithBlock: ^(AMPMiddlewarePayload * _Nonnull payload, AMPMiddlewareNext _Nonnull next) {
+        [payload.event setValue:eventType forKey:@"event_type"];
+        next(payload);
+    }];
+    AMPBlockMiddleware *swallowMiddleware = [[AMPBlockMiddleware alloc] initWithBlock: ^(AMPMiddlewarePayload * _Nonnull payload, AMPMiddlewareNext _Nonnull next) {
+    }];
+    [_middlewareRunner add:updateEventTypeMiddleware];
+    [_middlewareRunner add:swallowMiddleware];
+    
+    NSMutableDictionary *event = [NSMutableDictionary dictionary];
+    [event setValue:@"sample_event" forKey:@"event_type"];
+    [event setValue:@"sample_device" forKey:@"device_model"];
+    NSMutableDictionary *extra = [NSMutableDictionary dictionary];
+    
+    AMPMiddlewarePayload * middlewarePayload = [[AMPMiddlewarePayload alloc] initWithEvent:event extra:extra];
+    
+    __block BOOL middlewareCompleted = NO;
+    
+    [_middlewareRunner run:middlewarePayload next:^(AMPMiddlewarePayload *_Nullable newPayload){
+        middlewareCompleted = YES;
+    }];
+    
+    XCTAssertEqual(middlewareCompleted, NO);
+    XCTAssertEqualObjects([event objectForKey:@"event_type"], eventType);
 }
 
 @end
