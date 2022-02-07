ab09dde063d92e810411990a4d02c0676238aa8e
Brian Giori <brian.giori@amplitude.com>
2022-02-07 09:09:23 -0800

    fix: update analytics connector dependency in package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .library(name: "Amplitude", targets: ["Amplitude"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/amplitude/amplitude-ios-core.git", from: "1.0.0")
+        .package(url: "https://github.com/amplitude/analytics-connector-ios.git", from: "1.0.0")
     ],
     targets: [
         .target(
